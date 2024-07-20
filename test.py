f10fb1cf2e66e32cc8e28ad668fd988538c2a545
yoko86 <koguchi.h.394@ms.saitama-u.ac.jp>
2024-07-20 14:40:44 +0000

    Add new feature

--- test.py
+++ test.py
@@ -6,7 +6,8 @@ def example1():
     some_variable={'long':'Long code lines should be wrapped within 79 characters.',
     'other':[math.pi, 100,200,300,9876543210,'This is a long string that goes on'],
     'more':{'inner':'This whole logical line should be wrapped.',some_tuple:[1,
-    20,300,40000,500000000,60000000000000000]}}
+    20,300,40000,
+    500000000,60000000000000000]}}
     return (some_tuple, some_variable)
 def example2(): return {'has_key() is deprecated':True}.has_key({'f':2}.has_key(''));
 class Example3(   object ):
